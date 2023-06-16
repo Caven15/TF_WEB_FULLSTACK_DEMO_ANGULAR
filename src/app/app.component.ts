acfc351c5af39d4cc1682b23d799f67e4e895192
Caven15 <cavenditch15@gmail.com>
2023-06-16 11:00:38 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TF_WEB_FULLSTACK_DEMO_ANGULAR';
+}
